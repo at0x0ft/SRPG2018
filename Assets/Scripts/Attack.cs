a56fe2cc7e6479310ebbe5c7d21c02e231d2718d
25toma <2525toa@gmail.com>
2018-10-19 09:31:50 +0900

    [Fix] #23 Attack's class divide

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -3,21 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Attack : MonoBehaviour
+public abstract class Attack : MonoBehaviour
 {
-    public enum TargetingKind
-    {
-        Single,
-        Range
-    }
-
-    [SerializeField]
-    private TargetingKind _targetingWay;
-    public TargetingKind TargetingWay
-    {
-        get { return _targetingWay; }
-    }
-
+	// 攻撃の種類
     [SerializeField]
 	private Type _type;
 	public Type Type
@@ -25,14 +13,7 @@ public class Attack : MonoBehaviour
 		get { return _type; }
 	}
 
-    // 攻撃範囲を回転させられるかどうか
-    [SerializeField]
-    private bool _isRangeRotatable;
-    public bool IsRangeRotatable
-    {
-        get { return _isRangeRotatable; }
-    }
-
+	// 攻撃力
 	[SerializeField]
 	private int _power;
 	public int Power
@@ -40,7 +21,11 @@ public class Attack : MonoBehaviour
 		get { return _power; }
 	}
 
-	// atode kottini ikou shitai.
+	/// <summary>
+	/// 攻撃範囲.
+	/// SingleAttackの場合は、攻撃"可能"なマスの集合を示します。
+	/// RangeAttackの場合は、攻撃"する"マスの集合を示します。
+	/// </summary>
 	[SerializeField]
 	private List<Vector2Int> _range = new List<Vector2Int>
 	{
@@ -49,12 +34,13 @@ public class Attack : MonoBehaviour
 		new Vector2Int(-1, 0),
 		new Vector2Int(0, -1)
 	};
+	public List<Vector2Int> Range{ get { return _range; } }
+}
 
-	public List<Vector2Int> Range
-	{
-		get { return _range; }
-	}
 
+public class SingleAttack : Attack
+{
+	// 攻撃先の最小距離
 	[SerializeField]
 	private int _rangeMin;
 	public int RangeMin
@@ -62,10 +48,24 @@ public class Attack : MonoBehaviour
 		get { return _rangeMin; }
 	}
 
+	// 攻撃先の最大距離
 	[SerializeField]
 	private int _rangeMax;
 	public int RangeMax
 	{
 		get { return _rangeMax; }
 	}
+}
+
+
+public class RangeAttack : Attack
+{
+	// 攻撃範囲を回転させられるかどうか
+	[SerializeField]
+	private bool _isRotatable;
+	public bool IsRotatable
+	{
+		get { return _isRotatable; }
+	}
+
 }
