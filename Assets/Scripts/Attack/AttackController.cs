e10870d653be4da397c9dce5192d45970349bdc9
25toma <2525toa@gmail.com>
2018-10-22 02:34:39 +0900

    [Fix] for the BaseAttackController's params unknown problem, class devide

---
+++ Assets/Scripts/Attack/AttackController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+/// <summary>
+/// 制約：このクラスは、
+/// BaseAttackController
+/// SingleAttackController
+/// RangeAttackController
+/// の3つのクラスがアタッチされているGameObjectにアタッチすること。
+/// </summary>
+public class AttackController : MonoBehaviour
+{
+	[SerializeField]
+	private Map _map;
+	[SerializeField]
+	private Units _units;
+
+	private BaseAttackController _bac;
+	private SingleAttackController _sac;
+	private RangeAttackController _rac;
+
+	AttackController()
+	{
+		_bac = gameObject.GetComponent<BaseAttackController>();
+		_sac = gameObject.GetComponent<SingleAttackController>();
+		_rac = gameObject.GetComponent<RangeAttackController>();
+	}
+	
+	// バグ対策の、強制的な変更（隠蔽のため、このgetterは削除すること）
+	public BaseAttackController BAC
+	{
+		get{ return _bac; }
+	}
+
+	/// <summary>
+	/// ハイライトを行う
+	/// </summary>
+	/// <param name="map">便利な関数を色々呼び出すために使います</param>
+	/// <param name="attacker">攻撃主体</param>
+	/// <param name="attack">攻撃内容</param>
+	/// <param name="befDir">先程まで向いていた方角（任意）</param>
+	/// <param name="isClockwise">回転をする場合の方向</param>
+	/// <returns>単独攻撃:攻撃が出来るか否か, 範囲攻撃:攻撃する方角はどこか(東を0とした、反時計回り90°単位)</returns>
+	public int Highlight(Unit attacker, Attack attack, int befDir = -1, bool isClockwise = false)
+	{
+		if (attack.Scale==global::Attack.AttackScale.Single)
+		{
+			bool canAttack = _sac.SetAttackableHighlight(attacker, (SingleAttack)attack);
+			return (canAttack ? 1 : 0);
+		}
+		else if (attack.Scale==global::Attack.AttackScale.Range)
+		{
+			if (befDir == -1)
+			{
+				return _rac.InitializeAttackableHighlight(attacker, (RangeAttack)attack);
+			}
+			else
+			{
+				return _rac.UpdateAttackableHighlight(attacker, (RangeAttack)attack, befDir, isClockwise);
+			}
+		}
+		else
+		{
+			Debug.Log("予測されていない型の攻撃が行われました");
+			return -1;
+		}
+	}
+
+	/// <summary>
+	/// 攻撃を実行します
+	/// </summary>
+	/// <param name="map">便利関数を呼ぶため必要</param>
+	/// <param name="attacker">攻撃主体</param>
+	/// <param name="target">クリックされた攻撃先（マス座標）</param>
+	/// <param name="attack">攻撃内容</param>
+	/// <param name="units">便利関数を呼ぶため必要</param>
+	/// <returns>攻撃先に、そもそも敵が居たかどうか</returns>
+	public bool Attack(Unit attacker, Vector2Int target, Attack attack)
+	{
+		if (attack.Scale == global::Attack.AttackScale.Single)
+		{
+			return _sac.Attack(attacker, target, attack);
+		}
+		else if (attack.Scale == global::Attack.AttackScale.Range)
+		{
+			return _rac.Attack(attacker, attack);
+		}
+		else
+		{
+			Debug.Log("予定されていない型の攻撃がありました");
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/Attack/SingleAttackController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleAttackController : MonoBehaviour
+{
+	// ==========変数==========
+
+	[SerializeField]
+	private Map _map;
+	[SerializeField]
+	private Units _units;
+	private BaseAttackController _bac;
+
+
+	// ==========関数==========
+
+	private void Start()
+	{
+		_bac = gameObject.GetComponent<BaseAttackController>();	
+	}
+
+	/// <summary>
+	/// 攻撃可能なマスをハイライトする。
+	/// </summary>
+	/// <returns>攻撃対象が居るか否か(コマンド選択可否に使うのかな？)</returns>
+	public bool SetAttackableHighlight(Unit attacker, SingleAttack attack)
+	{
+		var hasTarget = false;
+		Floor startFloor = attacker.Floor;
+		foreach (var floor in _map.GetFloorsByDistance(startFloor, attack.RangeMin, attack.RangeMax))
+		{
+			if (floor.Unit == null || floor.Unit.Belonging == attacker.Belonging) continue;
+
+			// 取り出したマスにユニットが存在し, そのユニットが敵軍である場合
+			hasTarget = true;
+			floor.SetAttackableHighlight();
+		}
+		return hasTarget;
+	}
+
+	/// <summary>
+	/// 対象ユニットに攻撃
+	/// </summary>
+	/// <param name="target">攻撃先(マス座標)</param>
+	/// <returns>ユニットがあるかどうか</returns>
+	public bool Attack(Unit attacker, Vector2Int target, Attack attack)
+	{
+		var defender = _units.GetUnit(target.x, target.y);
+
+		if (defender == null) return false;
+
+		_bac.AttackToUnit(attacker, defender, attack);
+
+		_map.ClearHighlight();
+
+		_units.FocusingUnit.IsMoved = true;
+
+		return true;
+	}
+}
