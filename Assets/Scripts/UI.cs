67f86a212985272a5b20f5a1fd96923f4d2f8738
at0x0ft <at0p0ft@outlook.com>
2018-10-24 04:24:54 +0900

    Update debug code.

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -44,6 +44,7 @@ public class UI : MonoBehaviour
 	{
 		if(!_endCommandButton) Debug.LogError("[Error] : EndCommandButton is not set!");
 		if(!_touchBlocker) Debug.LogError("[Error] : Touch Blocker is not set!");
+		if(!_turnSetInfoWindow) Debug.LogError("[Error] : TurnSetInfoWindow is not set!");
 		if(!_unitInfoWindow) Debug.LogError("[Error] : UnitInfoWindow is not set!");
 	}
 
