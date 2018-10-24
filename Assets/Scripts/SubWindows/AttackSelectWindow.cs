9aa1f80b71e2d617f09803542834ac9faea6ae9f
at0x0ft <at0p0ft@outlook.com>
2018-10-24 19:15:51 +0900

    Add AttackSelectWindow.

---
+++ Assets/Scripts/SubWindows/AttackSelectWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using UnityEngine.UI;
+
+public class AttackSelectWindow : SubWindow
+{
+	[SerializeField]
+	private List<Button> _attackBtns;
+
+	/// <summary>
+	/// [SerializedField]で定義されたメンバがnullか否かを判定するメソッド (4debug)
+	/// </summary>
+	/// <returns></returns>
+	public void CheckSerializedMember()
+	{
+		foreach(var atkBtn in _attackBtns)
+		{
+			if(!atkBtn) Debug.LogError("[Error] : atkBtn is not fully set!");
+		}
+	}
+
+	public void Show(List<KeyValuePair<Attack, bool>> atkBoolPairs)
+	{
+		Hide();
+		foreach(var atkBtn in _attackBtns)
+		{
+			atkBtn.GetComponent<Text>().text = atkBoolPairs[0].Key.transform.name;
+			if(atkBoolPairs[0].Value)	// boolがtrueだったらボタンを有効化, そうじゃなければ, 攻撃防止. みたいな
+				atkBtn.gameObject.SetActive(true);
+		}
+
+		Show();
+	}
+}
