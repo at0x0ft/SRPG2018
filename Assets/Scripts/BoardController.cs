7a63c04b00f86c36fa2c3e3c424ae510fcb06ad4
at0x0ft <at0p0ft@outlook.com>
2018-10-24 11:05:22 +0900

    Update character order and rearrange the win/lose handling.

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -68,7 +68,7 @@ public class BoardController : MonoBehaviour
 
 
 		// endCommandボタンが押下されたらmapインスタンスメソッドの持つNextSet()を実行
-		_ui.EndCommandButton.onClick.AddListener(() => { NextSet(); });
+		_ui.EndCommandButton.onClick.AddListener(() => { NextUnit(); });
 
 		// AI設定
 		if(_setAI) SetAI(Unit.Team.Enemy, ac);
@@ -81,19 +81,41 @@ public class BoardController : MonoBehaviour
 		SetPlayerOrder();
 
 		// セット開始
-		StartSet(_startTeam);
+		StartPlayer(_startTeam);
 	}
 
 	/// <summary>
 	/// AIを設定する
 	/// </summary>
 	/// <param name="team"></param>
-	public void SetAI(Unit.Team team, AttackController ac)
+	private void SetAI(Unit.Team team, AttackController ac)
 	{
 		// AIインスタンスを初期化
 		_ai.Initialize(this, _map, _units, _moveController, ac);
 		// AIと相手プレイヤーを対応付ける
 		_ais[team] = _ai;
+	}
+
+	/// <summary>
+	/// セットを更新するメソッド
+	/// </summary>
+	private void UpdateSet()
+	{
+			Set++;
+
+			// 更に, セットが2以上ならば, ターン数も更新し, 移動量も補充.
+			if(Set > 2)
+			{
+				Turn++;
+				Set = 1;
+
+				// ターン開始時に、移動量を回復させる
+				foreach(var unit in _units.Characters)
+				{
+					unit.MoveAmount = unit.MaxMoveAmount;
+					Debug.Log("move amount:" + unit.MoveAmount);	// 4debug
+				}
+			}
 	}
 
 	/// <summary>
@@ -106,40 +128,44 @@ public class BoardController : MonoBehaviour
 	}
 
 	/// <summary>
+	/// 自軍の先頭のユニットを展開するメソッド.
+	/// </summary>
+	private void StartUnitAction()
+	{
+		// 自分の先頭のユニットを展開.
+		var activeUnit = _units.Order.FirstOrDefault();
+		if(!activeUnit) Debug.LogError("[Error] : " + _units.CurrentPlayerTeam.ToString() +  "'s active unit is not Found!");	// 4debug
+
+		// セットプレイヤーの先頭のユニット以外は行動済みとする
+		foreach(var unit in _units.Characters)
+		{
+			unit.IsMoved = unit != activeUnit;
+		}
+
+		// 盤面の状態を戦況確認中に設定
+		State = BattleState.CheckingStatus;
+
+		// Unitsクラスに記憶.
+		_units.ActiveUnit = activeUnit;
+	}
+
+	/// <summary>
 	/// セット開始時の処理
 	/// </summary>
 	/// <param name="team"></param>
-	public void StartSet(Unit.Team team)
+	private void StartPlayer(Unit.Team team)
 	{
-		// プレイヤーの順番が一巡したら, セット数・ターン数を更新
-		if(team == _startTeam)
-		{
-			Set++;
-			if(Set > 2)
-			{
-				Turn++;
-				Set = 1;
-			}
-		}
-
-		// 盤面の状態も設定
-		State = BattleState.CheckingStatus;
-
 		// セットプレイヤーのチームを記録
 		_units.CurrentPlayerTeam = team;
 
-		// セットプレイヤー以外ユニットは行動済みとする
-		foreach(var unit in _units.GetComponentsInChildren<Unit>())
-		{
-			unit.IsMoved = team != unit.Belonging;
-			//セット開始時に、移動量を回復させる
-			//(コードの場所がここで良いのかは、一考の余地あり)
-			if(!unit.IsMoved && Set == 1)
-			{
-				unit.MoveAmount = unit.MaxMoveAmount;
-				Debug.Log("move amount:" + unit.MoveAmount);
-			}
-		}
+		// プレイヤーの順番が一巡したら, セット数・ターン数を更新
+		if(_units.CurrentPlayerTeam == _startTeam) UpdateSet();
+
+		// セットプレイヤーのユニットの順番を設定
+		_units.SetUnitsOrder();
+
+		// セットプレイヤーの持つユニットのうち先頭のユニットを展開.
+		StartUnitAction();
 
 		// セットプレイヤーがAIならば, 画面をタッチできないように設定し, AIを走らせる.
 		if(_ais.ContainsKey(team))
@@ -155,16 +181,31 @@ public class BoardController : MonoBehaviour
 			Debug.Log("touch blocker invalid.");
 		}
 
-		Debug.Log("Arrange Finished."); // 4debug
+
+		Debug.Log("Player update Finished."); // 4debug
 	}
 
 	/// <summary>
-	/// 次のセットに更新
+	/// 次のユニットの行動に移る.
 	/// </summary>
-	public void NextSet()
+	public void NextUnit()
 	{
-		// 次のTeamの設定
+		// 行動が終了したユニットは順番から取り除く.
+		_units.Order.Remove(_units.ActiveUnit);
+
+		// まだ自軍のユニットが残っているのならば, 次のユニットに交代
+		if(_units.Order.Count > 0) StartUnitAction();
+		// 自軍のユニット全てが行動終了したならば, 次のプレイヤーに交代
+		else NextPlayer();
+	}
+
+	/// <summary>
+	/// 次のプレイヤーに更新
+	/// </summary>
+	private void NextPlayer()
+	{
+		// 次のTeamの設定 (現在対戦人数2人の時の場合のみを想定した実装)
 		var nextTeam = _units.CurrentPlayerTeam == Unit.Team.Player ? Unit.Team.Enemy : Unit.Team.Player;
-		StartSet(nextTeam);
+		StartPlayer(nextTeam);
 	}
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -261,6 +261,9 @@ public class Unit : MonoBehaviour
 		if(Life <= 0) DestroyWithAnimate();
 	}
 
+	/// <summary>
+	/// ユニットを消滅させるメソッド
+	/// </summary>
 	public void DestroyWithAnimate()
 	{
 		GetComponent<Button>().enabled = false;
@@ -268,5 +271,8 @@ public class Unit : MonoBehaviour
 			{
 				Destroy(gameObject);
 			});
+
+		// 勝敗判定を行い, 負けた場合はゲーム終了.
+		if(_units.JudgeLose(Belonging)) _units.FinishGame(Belonging);
 	}
 }
