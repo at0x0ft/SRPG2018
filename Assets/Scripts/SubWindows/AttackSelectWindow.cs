7db8f8a8d7f3b55dcd24919e811ed7a1b7880fa3
at0x0ft <at0p0ft@outlook.com>
2018-10-26 18:12:50 +0900

    Formatted the code.

--- Assets/Scripts/SubWindows/AttackSelectWindow.cs
+++ Assets/Scripts/SubWindows/AttackSelectWindow.cs
@@ -15,7 +15,7 @@ public class AttackSelectWindow
 	private AttackInfoWindow _attackInfoWindow;
 	private AttackController _attackController;
 	private Units _units;
-	
+
 	public void Initialize(Units units, AttackController attackController, AttackInfoWindow attackInfoWindow)
 	{
 		_attackInfoWindow = attackInfoWindow;
@@ -27,33 +27,43 @@ public class AttackSelectWindow
 	/// [SerializedField]で定義されたメンバがnullか否かを判定するメソッド (4debug)
 	/// </summary>
 	/// <returns></returns>
-	public void CheckSerializedMember()
+	public void CheckSerializedMember(List<KeyValuePair<Attack, bool>> atkBoolPairs)
 	{
 		foreach(var atkBtn in _attackBtns)
 		{
 			if(!atkBtn) Debug.LogError("[Error] : atkBtn is not fully set!");
 		}
+
+		if(atkBoolPairs.Count != _attackBtns.Count) Debug.LogError("[Error] : atkBtn and attack number does not match!");
 	}
 
 	public void Show(List<KeyValuePair<Attack, bool>> atkBoolPairs)
 	{
 		Hide();
+
+		CheckSerializedMember(atkBoolPairs);	// 4debug
+
+		// AttackSelectWindow内の攻撃のButtonを一度全て無効化する.
 		foreach(var button in _attackBtns)
 		{
 			button.gameObject.SetActive(false);
 		}
-		for(int i=0;i<atkBoolPairs.Count();i++)
+
+		for(int i = 0; i < atkBoolPairs.Count(); i++)
 		{
 			var atk = atkBoolPairs[i].Key;
-			var can = atkBoolPairs[i].Value;
-			Debug.Log(atk);
-			Debug.Log(atk.name);
-			Debug.Log(can);
-			_attackBtns[i].gameObject.SetActive(can);
-			_attackBtns[i].transform.Find("Name").GetComponent<Text>().text = atk.name;
+			var canAttack = atkBoolPairs[i].Value;
+			Debug.Log(atk);	// 4debug
+			Debug.Log(atk.name);	// 4debug
+			Debug.Log(canAttack);	// 4debug
+
+			// 有効な攻撃のみ, ウィンドウに表示し, 追加する.
+			_attackBtns[i].gameObject.SetActive(canAttack);
+			_attackBtns[i].GetComponentInChildren<Text>().text = atk.name;
 			_attackBtns[i].onClick.AddListener(() => _attackInfoWindow.Show(atk));
 			_attackBtns[i].onClick.AddListener(() => _attackController.Highlight(_units.ActiveUnit, atk));
 		}
+
 		Show();
 	}
 }
