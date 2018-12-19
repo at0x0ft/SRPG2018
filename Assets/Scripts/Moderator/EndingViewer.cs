3333005b28f331855408efdf7059c62e8a05be27
at0x0ft <at0p0ft@outlook.com>
2018-12-19 19:26:57 +0900

    Update .gitignore for the ending credit file.

---
+++ Assets/Scripts/Moderator/EndingViewer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System;
+using UnityEngine.UI;
+
+public class EndingViewer : MonoBehaviour
+{
+	[SerializeField]
+	Text _showingText;
+
+	private void Start()
+	{
+		if(!_showingText) Debug.LogError("[Error] : Showing Text is not set!");
+
+		InitializeText();
+	}
+
+	private void Update()
+	{
+
+	}
+
+	private void InitializeText()
+	{
+		_showingText.color = Color.white;
+	}
+
+}
