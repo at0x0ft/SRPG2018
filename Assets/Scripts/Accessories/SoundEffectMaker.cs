70995b06a22fe3653ea2ecc65078e050245ad183
25toma <2525toa@gmail.com>
2018-12-20 19:50:34 +0900

    [feat] sound effect script

---
+++ Assets/Scripts/Accessories/SoundEffectMaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundEffect
+{
+	Confirm,
+	Cancel,
+	HitOrDestroy,
+	Walk
+}
+
+public class SoundEffectMaker : MonoBehaviour {
+
+	List<AudioSource> sounds;
+
+	void Start () 
+	{
+		sounds = new List<AudioSource>();
+
+		var soundsObj = GetComponentsInChildren<AudioSource>();
+		int kind = soundsObj.Length;
+		for(int i=1;i<kind;i++)
+		{
+			sounds.Add(soundsObj[i]);
+		}
+	}
+	
+	public void play(SoundEffect kind)
+	{
+		var sound = sounds[(int)kind];
+
+		//if(sound.isPlaying) return;
+		sound.PlayOneShot(sound.clip);
+	}
+}
