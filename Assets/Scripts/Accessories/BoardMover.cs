591d9be587fe7375c5f9cbaad5d10b2dc5be072e
25toma <2525toa@gmail.com>
2018-12-21 13:18:07 +0900

    [add] forgut files

--- Assets/Scripts/Accessories/BoardMover.cs
+++ Assets/Scripts/Accessories/BoardMover.cs
@@ -10,11 +10,13 @@ public class BoardMover : MonoBehaviour {
 	Transform _sliders;
 
 	Slider _X, _Y, _Size, _speed;
-	Vector2 _boardSize;
+	Vector2 _boardSize, mapLDlim, mapRUlim;
 
 	private void Start () 
 	{
 		SetUpBoard();
+		SetUpTrimmer();
+		SetUpTouchBlocker();
 		SetUpSlider();
 		SetUpToggle();
 		SetUpButton();
@@ -31,6 +33,26 @@ public class BoardMover : MonoBehaviour {
 		trigger.triggers.Add(BoardScroll());
 	}
 
+	private void SetUpTrimmer()
+	{
+		var par = GameObject.Find("Trimmer").transform;
+		float ulim = par.Find("up").localPosition.y;
+		float llim = par.Find("left").localPosition.x;
+		float dlim = par.Find("down").localPosition.y;
+		float rlim = par.Find("right").localPosition.x;
+
+		mapLDlim = new Vector2(llim, dlim);
+		mapRUlim = new Vector2(rlim, ulim);
+	}
+
+	private void SetUpTouchBlocker()
+	{
+		// 非アクティブになってるので、親から子を取得する
+		var trigger = GameObject.Find("Canvas").transform.Find("TouchBlocker").GetComponent<EventTrigger>();
+		trigger.triggers.Add(BoardDrag());
+		trigger.triggers.Add(BoardScroll());
+	}
+
 	private EventTrigger.Entry BoardDrag()
 	{
 		var entry = new EventTrigger.Entry
@@ -39,11 +61,19 @@ public class BoardMover : MonoBehaviour {
 		};
 		entry.callback.AddListener((data) =>
 		{
-			var delta = ((PointerEventData)data).delta;
-			delta.x /= _boardSize.x / 2;
-			delta.y /= _boardSize.y / 2;
-			_X.value -= delta.x;
-			_Y.value += delta.y;
+			var pdata = data as PointerEventData;
+			var delta = pdata.delta;
+			var judge = IsInBoard(pdata.position);
+			if(judge.Key)
+			{
+				delta.x /= _boardSize.x / 2;
+				_X.value -= delta.x;
+			}
+			if(judge.Value)
+			{
+				delta.y /= _boardSize.y / 2;
+				_Y.value += delta.y;
+			}
 		});
 		return entry;
 	}
@@ -56,11 +86,60 @@ public class BoardMover : MonoBehaviour {
 		};
 		entry.callback.AddListener((data) =>
 		{
-			var delta = ((PointerEventData)data).scrollDelta;
-			Debug.Log(delta);
-			_Size.value += delta.y / 10;
+			var pdata = data as PointerEventData;
+			var judge = IsInBoard(pdata.position);
+			if(judge.Key && judge.Value)
+			{
+				var delta = pdata.scrollDelta;
+				_Size.value += delta.y / 10;
+			}
 		});
 		return entry;
+	}
+
+	private KeyValuePair<Vector2,Vector2> GetBoardRange()
+	{
+		float scale = _board.transform.localScale.x;
+		var size = _boardSize * scale;
+		Vector2 center = _board.transform.localPosition;
+
+		var lu = center - size / 2;
+		var rd = center + size / 2;
+		var res = new KeyValuePair<Vector2, Vector2>(lu, rd);
+		return res;
+	}
+
+	private KeyValuePair<bool,bool> IsInBoard(Vector2 now)
+	{
+		bool resX = true;
+		bool resY = true;
+
+		// preprocess
+		now.x -= Screen.width / 2;
+		now.y -= Screen.height / 2;
+		now.y *= -1;
+
+		// 1. front of board
+		var range = GetBoardRange();
+		var lu = range.Key;
+		var rd = range.Value;
+
+		Debug.Log(now);
+		Debug.Log(range);
+		Debug.Log(mapLDlim + " " + mapRUlim);
+
+		if(now.x < lu.x) resX = false;
+		if(now.y < lu.y) resY = false;
+		if(rd.x < now.x) resX = false;
+		if(rd.y < now.y) resY = false;
+
+		// 2. center of window
+		if(now.x < mapLDlim.x) resX = false;
+		if(now.y < mapLDlim.y) resY = false;
+		if(mapRUlim.x < now.x) resX = false;
+		if(mapRUlim.y < now.y) resY = false;
+
+		return new KeyValuePair<bool, bool>(resX, resY);
 	}
 
 	private void SetUpSlider()
